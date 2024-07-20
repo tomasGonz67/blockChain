0cd4b3432308467de24533949f93c5b6de514a3a
Thomas-Gonz67 <thomasgonz67@gmail.com>
2024-07-20 04:26:59 -0400

    added and fixed bugs. finally got a working contact

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const SEPOLIA_PRIVATE_KEY ="a8bd8eea26f30b14752864fd9c0a36d27a3f3dee1192360a40ce4e11e2f470df"; // Ensure this matches .env
+
+console.log("INFURA_API_KEY:", INFURA_API_KEY); // Should log the API key or undefined
+console.log("SEPOLIA_PRIVATE_KEY:", SEPOLIA_PRIVATE_KEY); // Should log the private key or undefined
 
 module.exports = {
     solidity: "0.8.24",
     networks: {
-        hardhat: {
-            chainId: 1337,
+        sepolia: {
+            url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+            accounts: [`0x${SEPOLIA_PRIVATE_KEY}`], // Ensure private key is correct
         },
     },
 };
